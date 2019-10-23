a8a01ad536c492097a50342b626eb73858b4938c
EC2 Default User <ec2-user@ip-172-31-31-167.us-west-2.compute.internal>
2019-10-23 15:03:17 +0000

    created much of the structure and logic

--- boggle.py
+++ boggle.py
@@ -1,8 +1,90 @@
+from string import ascii_uppercase
+from random import choice
 
 def make_grid(width, height):
     """
     Creates a Boggle grid that will hold all of the tiles for a game
     """
-    return {(row, col): ' ' for row in range(height)
+    return {(row, col): choice(ascii_uppercase) 
+        for row in range(height)
         for col in range(width)
+        
     }
+    
+def neighbors_of_position(coords):
+    """
+    Get neighbors of a given position
+    """
+    row = coords[0]
+    col = coords[1]
+    
+    #Assign each of the neighbors
+    
+    #Top-left to top-right
+    top_left = (row - 1, col - 1)
+    top_center = (row - 1, col)
+    top_right = (row - 1, col + 1)
+    
+    #Left to right
+    left = (row, col - 1)
+    #The (row, col) coordinates passed to this 
+    #function are here
+    right = (row, col + 1)
+    
+    #Bottom-left to bottom-right
+    bottom_left = (row + 1, col - 1)
+    bottom_center = (row + 1, col)
+    bottom_right = (row + 1, col + 1)
+    
+    return [top_left, top_center, top_right,
+            left, right,
+            bottom_left, bottom_center, bottom_right]
+            
+def all_grid_neighbors(grid):
+    """
+    Get all of the possible neighbors for each position inthe grid
+    """
+    neighbors = {}
+    for position in grid:
+        position_neighbors = neighbors_of_position(position)
+        neighbors[position] = [p for p in position_neighbors if p in grid]
+    return neighbors
+    
+def path_to_word(grid, path):
+    """
+    Add all the letters on the path to a string
+    """
+    # print(grid)
+    # print(path)
+    return "".join([grid[p] for p in path])
+    
+def search(grid, dictionary):
+    """
+    Search through the paths to locate words by matching
+    strings to words in the dictionary
+    """
+    neighbors = all_grid_neighbors(grid)
+    paths = []
+    
+    def do_search(path):
+        word = path_to_word(grid, path)
+        if word in dictionary:
+            paths.append(path)
+        for next_pos in neighbors[path[-1]]:
+            if next_pos not in path:
+                do_search(path + [next_pos])
+                
+    for position in grid:
+        do_search([position])
+            
+    words = []
+    for path in paths:
+        words.append(path_to_word(grid, path))
+    return set(words)
+        
+def getDictionary(dictionary_file):
+    """
+    Load dictionary file
+    """
+    with open(dictionary_file) as f:
+        return [w.strip().upper() for w in f]
--- test_boggle.py
+++ test_boggle.py
@@ -1,5 +1,6 @@
 import unittest
 import boggle
+from string import ascii_uppercase
 
 class TestBoggle(unittest.TestCase):
     """
@@ -32,3 +33,75 @@ class TestBoggle(unittest.TestCase):
         self.assertIn((1, 0), grid)
         self.assertIn((1, 1), grid)
         self.assertNotIn((2, 2), grid)
+        
+    def test_grid_is_filled_with_letters(self):
+        """
+        Ensure that each of the coordinates in the 
+        grid conatins letters
+        """
+        grid = boggle.make_grid(2,3)
+        for letter in grid.values():
+            self.assertIn(letter, ascii_uppercase)
+            
+    def test_neighbors_of_a_position(self):
+        """
+        Ensure that a position has 8 neighbors
+        """
+        coords = (1, 2)
+        neighbors = boggle.neighbors_of_position(coords)
+        self.assertIn((0, 1), neighbors)
+        self.assertIn((0, 2), neighbors)
+        self.assertIn((0, 3), neighbors)
+        self.assertIn((1, 1), neighbors)
+        self.assertIn((1, 3), neighbors)
+        self.assertIn((2, 1), neighbors)
+        self.assertIn((2, 2), neighbors)
+        self.assertIn((2, 3), neighbors)
+        
+    def test_all_grid_neighbors(self):
+        """
+        Ensure that all the grid positions have neighbors
+        """
+        grid = boggle.make_grid(2, 2)
+        neighbors = boggle.all_grid_neighbors(grid)
+        self.assertEqual(len(neighbors), len(grid))
+        
+        for position in grid:
+            others = list(grid) #creates a new list from the dictionary's keys
+            others.remove(position)
+            self.assertListEqual(sorted(neighbors[position]), sorted(others))
+            
+    def test_converting_a_path_to_a_word(self):
+        """
+        Ensure that paths can be converted to words
+        """
+        grid = boggle.make_grid(2, 2)
+        oneLetterWord = boggle.path_to_word(grid, [(0, 0)])
+        twoLetterWord = boggle.path_to_word(grid,[(0, 0), (1, 1)])
+        self.assertEqual(oneLetterWord, grid[(0, 0)])
+        self.assertEqual(twoLetterWord, grid[(0, 0)] + grid[(1, 1)])
+        
+    def test_search_grid_for_words(self):
+        """
+        Ensure that certain patterns can be found in a path_to_word
+        """
+        grid = {(0, 0): 'A', (0, 1): 'B', (1, 0): 'C', (1, 1): 'D'}
+        twoLetterWord = 'AB'
+        threeLetterWord = 'ABC'
+        notThereWord = 'EEE'
+        dictionary = [twoLetterWord, threeLetterWord, notThereWord]
+        
+        foundWords = boggle.search(grid, dictionary)
+        
+        self.assertTrue(twoLetterWord in foundWords)
+        self.assertTrue(threeLetterWord in foundWords)
+        self.assertTrue(notThereWord not in foundWords)
+        
+    def test_load_dictionary(self):
+        """
+        Test that the 'get dictionary' function returns a dictionary
+        that has a  length greater than 0
+        """
+        dictionary = boggle.getDictionary('words.txt')
+        self.assertGreater(len(dictionary), 0)
+        
